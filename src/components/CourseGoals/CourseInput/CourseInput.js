e28e69c7cfedbdc7251a64c3956939a925818874
Suthan83 <suthan83@gmail.com>
2022-01-15 18:23:06 -0500

    move inline styles css file and conver to use conditional style clases inline

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -27,9 +27,9 @@ const CourseInput = props => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
-        <label style={{color: isValid ? '' : 'red'}}>Course Goal</label>
-        <input style= {{borderColor: isValid ? '' : 'red', backgroundColor: isValid ? '' : 'pink'}} type="text" onChange={goalInputChangeHandler} />
+      <div className={`form-control ${isValid? '':'invalid'}`}>
+        <label>Course Goal</label>
+        <input type="text" onChange={goalInputChangeHandler} />
       </div>
       <Button type="submit">Add Goal</Button>
     </form>
